85c0a440057f34137e62de507b20a1e10add5377
Genilson de Maceno <genilson_de_maceno@hotmail.com>
2020-10-13 22:41:45 -0300

    Atividades C#

---
+++ Array e tipos genéricos/AluraStudio-CSharpPT5-b0bdb1e09d442e7dd25ca018d02d2ad5ce9c4405/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,55 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // açucar sintatico
+            int[] idade = new int[]
+            {
+                15,28,35,50,28
+            };
+
+            //idade[0] = 15;
+            //idade[1] = 28;
+            //idade[2] = 35;
+            //idade[3] = 50;
+            //idade[4] = 28;
+            //idade[5] = 32;
+
+            int acumulador = 0;
+            for (int indice = 0; indice < idade.Length; indice++)
+            {
+
+                acumulador += idade[indice];
+
+                //int idades = idade[indice];
+                ////Console.WriteLine("idade.Length " + idade.Length);
+                ////Console.WriteLine($"Valor no índice {i}: {idade}");
+                Console.WriteLine($"valor no índice {indice}: {idade[indice]}");
+            }
+            int media = acumulador / idade.Length;
+            Console.WriteLine($"media {media}");
+            
+            
+            //int indice = 2;
+            //int valorNoIndice = idade[indice];
+            //int outroValor = idade[2 + 2];
+           
+
+
+            //Console.WriteLine("idade " + indice);
+            //Console.WriteLine("idade " + valorNoIndice);
+            //Console.WriteLine("idade " + outroValor);
+
+            Console.ReadLine();
+        }
+    }
+}
